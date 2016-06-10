673d0c233159b4083cc91a34c91a9c9f744c20a7
lucasgr7 <lucasgr7@gmail.com>
2016-06-09 22:45:30 -0300

    Inlcuido array de mensagens, e variação na mensagem apresentada e encerando o browser

--- Source/Mapping.py
+++ Source/Mapping.py
@@ -3,6 +3,8 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait # available since 2.4.0
 from selenium.webdriver.support import expected_conditions as EC # available since 2.26.0
+import json
+import random
 
 class FacebookMap(object):
 
@@ -23,6 +25,14 @@ class FacebookMap(object):
 		btnEntrar.click()
 
 	def EnviarParabens(self):
+		try:
+			with open('resource.json') as data_file:
+				data = json.load(data_file)
+		except:
+			print("Você precisa ter um arquivo de mensagens de parabéns resource.json")
+			data = []
+			data.append("Feliz aniversário %s tudo de bom e felicidades :)")
+
 		self.browser.get('https://www.facebook.com/events/birthdays')
 		nomes_aniversariantes = self.browser.find_elements_by_class_name('_3ng2')
 		nomes = []
@@ -34,7 +44,11 @@ class FacebookMap(object):
 		x = 0
 		for text_box in texts_boxes:
 			if(x <= len(texts_boxes)):
-				text_box.send_keys('Feliz aniversário ' + nomes[x] + " tudo de bom e felicidades :)")
+				mensagem = data[random.randrange(0,len(data))] % nomes[x]
+				text_box.send_keys(mensagem)
 				text_box.send_keys(Keys.RETURN)
 
 			x += 1
+
+	def Encerrar(self):
+		self.browser.close()
